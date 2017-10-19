202f3587f0fd6562dd2a9d2487ecade77bb730df
Slava Shevchenko <kaganvmz@gmail.com>
2017-10-19 22:03:58 +0300

    add gitignore and markup to app.js

--- app.js
+++ app.js
@@ -2,6 +2,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 
-const PI = 3.14;
+function Application() {
+    return (
+        <div className="scoreboard">
+           <div className="header">
+               <h1>Scoreboard</h1>
+           </div>
 
-console.log(PI);
+           <div className="players">
+               <div className="player">
+                   <div className="player-name">
+                       Jim Hoskins
+                   </div>
+                   <div className="player-score">
+                       <div className="counter">
+                            <button className="counter-action decrement"> - </button>
+                            <div className="counter-score"> 31 </div>
+                            <button className="counter-action increment"> + </button>
+                       </div>
+                   </div>
+               </div>
+           </div>
+        </div>
+    );
+}
+
+ReactDOM.render(<Application />, document.getElementById('app'));
